6142256c621ee0f4fb9e0ff824a871ff47757f8b
nammyatyuu <namichan2533@yahoo.co.jp>
2024-09-26 19:41:09 +0900

    変更

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -60,7 +60,10 @@
                                 <div class="col-12">
                                     <p class="samuraimart-product-label mt-2">
                                         <a href="{{ route('products.show', $recently_product) }}" class="link-dark">{{ $recently_product->name }}</a>
-                                        <br>
+                                        @if ($recently_product->reviews()->exists())
+                                         <span class="samuraimart-star-rating" data-rate="{{ round($recently_product->reviews->avg('score') * 2) / 2 }}"></span>
+                                          {{ round($recently_product->reviews->avg('score'), 1) }}<br>
+                                        @endif
                                         <label>￥{{ number_format($recently_product->price) }}</label>
                                     </p>
                                 </div>
